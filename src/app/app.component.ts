710ff3a7b964ae7063136856c7552272845d60ab
baliboy20 <williampaulton@yahoo.co.uk>
2018-10-11 14:22:28 +0100

    thurs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'zz14';
+export class AppComponent implements OnInit  {
+
+    flag = false;
+    @ViewChild('player') player: ElementRef;
+    ngOnInit() {
+       // this.flag = true;
+        const player: HTMLVideoElement = this.player.nativeElement as HTMLVideoElement;
+        player.load();
+        console.log('ng onit play video');
+        player.playbackRate = 10;
+        player.onended = () => this.flag = false;
+       // player.play();
+    }
+    // @HostListener('onload', ['$event.target'])
+    load(event) {
+        // const player: HTMLVideoElement = this.player.nativeElement as HTMLVideoElement;
+        console.log('play video'); //fjsd
+        // player.play();
+    }
+
+
+    vidFinish(event) {
+        console.log('video finished');
+    }
 }
