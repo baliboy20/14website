3d03d8999951f6d96146dc7278e70b70d51c365c
baliboy20 <williampaulton@yahoo.co.uk>
2018-10-17 14:27:41 +0100

    + pics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,32 +5,50 @@ import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/c
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit  {
-showvid = true;
+export class AppComponent implements OnInit {
+    showvid = true;
     flag = false;
     @ViewChild('player') player: ElementRef;
 
-  scrollup = (ev) => console.log(ev);
-  ngOnInit() {
-     window.addEventListener('scroll',this.scrollup)
-       this.flag = true;
+    scrollup = (ev) => console.log(ev);
+
+    ngOnInit() {
+        window.addEventListener('scroll', this.scrollup);
+        this.flag = true;
         const player: HTMLVideoElement = this.player.nativeElement as HTMLVideoElement;
         player.load();
-        player.playbackRate = 10;
+
+        player.playbackRate = 1;
         player.onended = () => this.flag = false;
-      if (this.showvid) {
-          player.play();
-      }
+        if (this.showvid) {
+            player.currentTime = 41;
+            player.play();
+        }
     }
-    // @HostListener('onload', ['$event.target'])
+
     load(event) {
-        // const player: HTMLVideoElement = this.player.nativeElement as HTMLVideoElement;
-        console.log('play video'); //fjsd
+        console.log('play video');
         // player.play();
+    }
+
+    closeVideoWindow() {
+        const player: HTMLVideoElement = this.player.nativeElement as HTMLVideoElement;
+        player.pause();
+        player.currentTime = 0;
+        this.showvid = false;
     }
 
 
     vidFinish(event) {
         console.log('video finished');
+    }
+
+    closeVidWindow() {
+        const player: HTMLVideoElement = this.player.nativeElement as HTMLVideoElement;
+        player.pause();
+        player.currentTime = 0;
+        this.showvid = false;
+        this.flag = false;
+        console.log('hello');
     }
 }
