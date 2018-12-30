78d702d83a637d6b5502637e57e71a1674caa42f
baliboy20 <williampaulton@yahoo.co.uk>
2018-12-30 13:37:47 +0100

    commit prior to aninmation branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,82 +4,91 @@ import {
   HostListener,
   OnInit,
   ViewChild,
-  isDevMode} from '@angular/core';
+  isDevMode
+} from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    showvid = true;
-    showFirstRow = true;
-    flag = true;
-    lastScrollPos = 0;
-    @ViewChild('player') player: ElementRef;
+  showvid = true;
+  showFirstRow = true;
+  flag = true;
+  lastScrollPos = 0;
+  @ViewChild('player') player: ElementRef;
+  @ViewChild('drinkvid') drinkvid: ElementRef;
+  @ViewChild('foodjourney') foodjourney: ElementRef;
 
-    scrollup = (ev) => console.log(ev);
+  scrollup = (ev) => console.log(ev);
 
-    ngAfterInit() {
+  ngAfterInit() {
 
-    }
+  }
 
-    ngOnInit() {
-        window.addEventListener('scroll', this.scrollup);
-        console.log('hello dolly');
-        this.flag = true;
-        if (isDevMode()) {
-            this.flag = true;
-            //git adreturn;
+  ngOnInit() {
+    window.addEventListener('scroll', this.scrollup);
+    console.log('hello dolly', this.drinkvid.nativeElement);
+    this.flag = true;
+    const drinkvid1: HTMLVideoElement = this.drinkvid.nativeElement as HTMLVideoElement;
+    drinkvid1.load();
+    drinkvid1.currentTime = 1;
+    const foodjoun: HTMLVideoElement = this.foodjourney.nativeElement as HTMLVideoElement;
+    foodjoun.load();
+    foodjoun.currentTime = 1;
 
-        }
-        console.log('INsie settimeout 9991');
-        setTimeout(() => {
-            const player: HTMLVideoElement = this.player.nativeElement as HTMLVideoElement;
-            player.load();
-console.log('INsie settimeout');
-            player.playbackRate = 1;
-            player.onended = () => this.flag = false;
-            if (this.showvid) {
-                console.log('this show vid is good');
-                player.currentTime = 1;
-                player.play();
-            }
-        }, 500);
+    if (isDevMode()) {
+      this.flag = false;
+      // git adreturn;
 
     }
+    setTimeout(() => {
+      const player: HTMLVideoElement = this.player.nativeElement as HTMLVideoElement;
+      player.load();
+      player.playbackRate = 1;
+      player.onended = () => this.flag = false;
+      if (this.showvid) {
+        console.log('this show vid is good');
+        player.currentTime = 1;
+        player.play();
+        // drink video
+      }
+    }, 500);
 
-    load(event) {
-        console.log('video loaded');
-        // player.play();
-    }
+  }
 
-    closeVideoWindow() {
-        const player: HTMLVideoElement = this.player.nativeElement as HTMLVideoElement;
-        player.pause();
-        player.currentTime = 2;
-        this.showvid = false;
-    }
+  load(event) {
+    console.log('video loaded');
+    // player.play();
+  }
+
+  closeVideoWindow() {
+    const player: HTMLVideoElement = this.player.nativeElement as HTMLVideoElement;
+    player.pause();
+    player.currentTime = 2;
+    this.showvid = false;
+  }
 
 
-    vidFinish(event) {
-        console.log('video finished');
-    }
+  vidFinish(event) {
+    console.log('video finished');
+  }
 
-    closeVidWindow() {
-        const player: HTMLVideoElement = this.player.nativeElement as HTMLVideoElement;
-        player.pause();
-        player.currentTime = 0;
-        this.showvid = false;
-        this.flag = false;
-        console.log('hello' + this.flag);
-    }
+  closeVidWindow() {
+    const player: HTMLVideoElement = this.player.nativeElement as HTMLVideoElement;
+    player.pause();
+    player.currentTime = 0;
+    this.showvid = false;
+    this.flag = false;
+    console.log('hello' + this.flag);
+  }
 
-    scrollToElement(ele) {
-        console.log(ele);
-        const html: HTMLHtmlElement = ele as HTMLHtmlElement;
-        console.log(html);
-        html.scrollIntoView({behavior: 'smooth'});
-        // window.scrollBy(0, -150);
-    }
+  scrollToElement(ele) {
+    console.log(ele);
+    const html: HTMLHtmlElement = ele as HTMLHtmlElement;
+    console.log(html);
+    html.scrollIntoView({behavior: 'smooth'});
+    // window.scrollBy(0, -150);
+  }
 }
